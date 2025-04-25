cb2cd43e1bdc3299096fbec1d4c3aef49a385e16
Almir Neto <almirneto338@gmail.com>
2025-04-25 16:37:43 -0300

    feat: adding lomuto partition from last to first

--- exercise-2.2/reduce-and-conquer/lomuto_partition.py
+++ exercise-2.2/reduce-and-conquer/lomuto_partition.py
@@ -1,9 +1,15 @@
-def lomuto_partition(items: list[int]):
-    s = 0
-    pivot = items[0]
-    for i in range(1, len(items)):
-        if items[i] <= pivot:
-            s += 1
-            items[i], items[s] = items[s], items[i]
-    items[s], items[0] = items[0], items[s]
-    return s
+def lomuto_partition(
+    items: list[int],
+    left: int,
+    right: int,
+):
+    pivot = items[right]
+    i = left - 1
+
+    for j in range(left, right):
+        if items[j] <= pivot:
+            i += 1
+            items[i], items[j] = items[j], items[i]
+    right_place = i + 1
+    items[right_place], items[right] = items[right], items[right_place]
+    return right_place
