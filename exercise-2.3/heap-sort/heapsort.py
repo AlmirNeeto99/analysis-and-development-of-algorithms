4f6854141e363550c20367e1f411929c66c1948a
Almir Neto <almirneto338@gmail.com>
2025-05-15 23:49:29 -0300

    fixing max heapify

--- exercise-2.3/heap-sort/heapsort.py
+++ exercise-2.3/heap-sort/heapsort.py
@@ -1,23 +1,26 @@
 def get_max(arr, left, right):
     arr_len = len(arr)
-    if left >= arr_len or right >= arr_len:
-        return -1
-
+    if left >= arr_len:
+        return arr_len
+    if right >= arr_len:
+        return left
     if arr[left] >= arr[right]:
         return left
     return right
 
 
-def max_heapify(arr, n, i):
+def max_heapify(arr, size, i):
     c = get_max(arr, 2 * i + 1, 2 * i + 2)
-    while c != -1 and c < len(arr) and arr[c] > arr[i]:
+    while c < size and arr[c] > arr[i]:
         arr[i], arr[c] = arr[c], arr[i]
         i = c
         c = get_max(arr, 2 * i + 1, 2 * i + 2)
 
 
 def build_max_heap(arr):
-    pass
+    arr_len = len(arr)
+    for i in range(arr_len // 2 - 1, -1, -1):
+        max_heapify(arr, arr_len, i)
 
 
 def heapsort(arr):
